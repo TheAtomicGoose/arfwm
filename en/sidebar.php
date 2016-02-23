d43021cbaa82234f2361c5620949c9e9abe0d1cc
Jesse Evers <jesse27999@gmail.com>
2016-02-22 19:45:02 -0500

    added facebook link

--- en/sidebar.php
+++ en/sidebar.php
@@ -2,4 +2,7 @@
         ARFWM provides direct financial assistance to women and girls seeking abortions who cannot afford them. Our primary commitment is to women and girls in the four counties of Western Massachusetts.
     <br><br><br>
     <div id="quote"></div>
+    <div id="facebook">
+        <a href="https://www.facebook.com/Abortion-Rights-Fund-of-Western-Massachusetts-311061232283840"><img src="../images/facebook.png" class="img-responsive"></a>
+    </div>
 </div>
--- es/sidebar.php
+++ es/sidebar.php
@@ -2,4 +2,7 @@
     ARFWM provee ayuda financiera directamente a mujeres y ni&ntilde;as que procuran un aborto y no pueden pagar por el. Nuestro compromiso principal es con las mujeres y ni&ntilde;as en los cuatro condados del oeste de Massachusetts.
     <br><br><br>
     <div id="quote"></div>
+    <div id="facebook">
+        <a href="https://www.facebook.com/Abortion-Rights-Fund-of-Western-Massachusetts-311061232283840"><img src="../images/facebook.png" class="img-responsive"></a>
+    </div>
 </div>
